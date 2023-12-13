a1fcdc6e0e6e1eb7e12175c903f0271598b88b9d
Hacker God <jyrone.parker@gmail.com>
2023-12-12 19:07:28 -0500

    added Laravel Breeze and Laravel Cashier

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -19,11 +19,19 @@ return Application::configure()
     ->withProviders()
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
-        // api: __DIR__.'/../routes/api.php',
+        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         // channels: __DIR__.'/../routes/channels.php',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->api(prepend: [
+            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+        ]);
+
+        $middleware->alias([
+            'verified' => \App\Http\Middleware\EnsureEmailIsVerified::class,
+        ]);
+
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
